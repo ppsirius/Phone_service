246a5f7b667d3466fbda3b0e746e977c240f653a
Piotr Pupczyk <piotrek@jakbyco.com>
2014-10-19 14:39:27 +0200

    working state machine

--- app/models/repair.rb
+++ app/models/repair.rb
@@ -5,4 +5,19 @@ class Repair < ActiveRecord::Base
 
   validates :phone_model, :client, :status, :imei, :description,  presence: true
 
+  state_machine :status, :initial => :in_repair do 
+
+    event :fix do 
+      transition :in_repair => :fixed, :sent_to_service => :fixed, :not_repairable => :fixed
+    end
+
+    event :send_to_service do 
+      transition :in_repaire => :sent_to_service
+    end
+
+    event :cant_repair do 
+      transition :in_repaire => :not_repairable, :sent_to_service => :not_repairable
+    end
+  end
+
 end
